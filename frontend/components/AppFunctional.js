69e1912241ff79fa67838150ac973e8ed213dc62
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-03-28 10:29:50 +0300

    polish and fix for mvp.test

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 
-// önerilen başlangıç stateleri
 const initialMessage = ''
 const initialEmail = ''
 const initialSteps = 0
-const initialIndex = 4 //  "B" nin bulunduğu indexi
+const initialIndex = 4
 
 const grid = [
   { id: 0, x: 1, y: 1 },
@@ -23,30 +22,22 @@ const xCount = 3
 const yCount = 3
 
 export default function AppFunctional(props) {
-  // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
-  // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
   const [activeSquare, setActiveSquare] = useState(initialIndex)
   const [steps, setSteps] = useState(initialSteps)
   const [message, setMessage] = useState(initialMessage)
   const [email, setEmail] = useState(initialEmail)
 
   function getXY() {
-    // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
-    // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
     return [grid[activeSquare].x, grid[activeSquare].y]
   }
 
   function getXYMesaj() {
-    // Kullanıcı için "Koordinatlar (2, 2)" mesajını izlemek için bir state'in olması gerekli değildir.
-    // Koordinatları almak için yukarıdaki "getXY" helperını ve ardından "getXYMesaj"ı kullanabilirsiniz.
-    // tamamen oluşturulmuş stringi döndürür.
     getXY()
     const mesaj = `Koordinatlar (${grid[activeSquare].x},${grid[activeSquare].y})`
     return mesaj
   }
 
   function reset() {
-    // Tüm stateleri başlangıç ​​değerlerine sıfırlamak için bu helperı kullanın.
     setActiveSquare(initialIndex)
     setSteps(initialSteps)
     setMessage(initialMessage)
@@ -54,9 +45,6 @@ export default function AppFunctional(props) {
   }
 
   function sonrakiIndex(yon) {
-    // Bu helper bir yön ("sol", "yukarı", vb.) alır ve "B" nin bir sonraki indeksinin ne olduğunu hesaplar.
-    // Gridin kenarına ulaşıldığında başka gidecek yer olmadığı için,
-    // şu anki indeksi değiştirmemeli.
     let nextSquareId = null
     if ((yon === 'left') & (grid[activeSquare].x !== 1)) {
       nextSquareId = grid[activeSquare].id - 1
@@ -73,8 +61,6 @@ export default function AppFunctional(props) {
   }
 
   function ilerle(evt) {
-    // Bu event handler, "B" için yeni bir dizin elde etmek üzere yukarıdaki yardımcıyı kullanabilir,
-    // ve buna göre state i değiştirir.
     const yon = evt.target.id
     const yonText = evt.target.name
     const nextSquareId = sonrakiIndex(yon)
@@ -89,12 +75,10 @@ export default function AppFunctional(props) {
   }
 
   function onChange(evt) {
-    // inputun değerini güncellemek için bunu kullanabilirsiniz
     setEmail(evt.target.value)
   }
 
   function onSubmit(evt) {
-    // payloadu POST etmek için bir submit handlera da ihtiyacınız var.
     evt.preventDefault()
     const dataSend = {
       x: grid[activeSquare].x,
